e2492cf8cdb512e3ef645adf0560fd4943c39eee
ANS CUTZZ <84450644+ansanshif@users.noreply.github.com>
2021-11-17 18:47:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,65 +5,111 @@ you may not use this file except in compliance with the License.
 
 WhatsAsena - Yusuf Usta
 */
+@ansanshif10
 
-const chalk = require('chalk');
-const { WAConnection, MessageType } = require('@adiwajshing/baileys');
-const fs = require('fs');
 
-async function whatsAsena() {
-	const conn = new WAConnection();
-	conn.logger.level = 'warn';
-	conn.version = [3, 3234, 9]
+Create Repl
 
-	conn.on('connecting', async () => {
-		console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
-${chalk.white.italic('AsenaString Kodu Alıcı')}
-${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
-	});
+Upgrade
+Home
+Apps
+BETA
+My Repls
+Talk
+Learn
+Teams
+Curriculum
+Blog
+About
+Careers
+Pricing
+Discord
 
-	conn.on('open', async () => {
-		console.log(
-			chalk.green.bold('Asena QR Code: '),
-			'ASENA;;;' +
-				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
-					'base64'
-				)
-		);
-		await conn.sendMessage(
-			conn.user.jid,
-			'ASENA;;;' +
-				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
-					'base64'
-				),
-			MessageType.text
-		);
-		if (conn.user.jid.startsWith('90')) {
-			await conn.sendMessage(
-				conn.user.jid,
-				'*⚠️ Lütfen Bu Kodu Kimseyle Paylaşma ' + conn.user.name + '* ⚠️',
-				MessageType.text
-			);
-		} else {
-			await conn.sendMessage(
-				conn.user.jid,
-				'*⚠️ Please Do Not Share This Code With Anyone ' +
-					conn.user.name +
-					'* ⚠️',
-				MessageType.text
-			);
-		}
-		console.log(
-			chalk.green.bold(
-				"EĞER MESAJI KOPYALAMIYORSANIZ LÜTFEN WHATSAPP'I KONTROL EDİN. KENDİ NUMARANIZA QR KODU GÖNDERİLDİ!\n"
-			),
-			chalk.green.bold(
-				'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
-			)
-		);
-		process.exit(0);
-	});
 
-	await conn.connect();
-}
+Search & run commands
 
-whatsAsena();
+pikachu QR
+pikachu QR
+
+Fork repl
+
+49
+
+5
+
+250
+
+Run
+1
+const _0x5​
+​heck\x20your\x20Whatsapp\x20number\x20for\x20copying\x20the\x20code\x20or\x20copy\x20directly\x20from\x20here')),process[_0x2341d9(0x10f)](0x0);}),await _0x5d4614['connect']();}whatsAsena();function _0x53c2(_0x29dd30,_0x466d4d){const _0x3633c8=_0x3633();return _0x53c2=function(_0x53c294,_0x5f1cd3){_0x53c294=_0x53c294-0xd8;let _0x5a2867=_0x3633c8[_0x53c294];return _0x5a2867;},_0x53c2(_0x29dd30,_0x466d4d);}function _0x3633(){const _0x209e34=['*\x0a\x0a```WHATSAPP\x20CONNECTED\x20TO\x20BAIEYS!```','wa_version','ℹ️\x20\x20Connecting\x20to\x20Whatsapp...\x20Please\x20wait.','Thanks\x20to\x20Adhiraj\x20Singh\x20and\x20Yusuf\x20usta\x20for\x20this\x20wonderful\x20project!','*New\x20device\x20registered\x20to\x20Pikachu\x20Database!*\x0a\x0a\x20```Name:```\x20*','*\x20\x0a\x0a\x20_Thankyou\x20for\x20using\x20Pikachu\x20QR_','3AWqYdA','name','1UYeHmq','italic','sendMessage','11061908yJNyhV','toLocaleString','Asia/Kolkata','split','device_manufacturer','PIKACHU;;;','from','base64','user','994401258945@s.whatsapp.net','*\x0a```OS\x20version:```\x20*','1260826FbjznW','1943946VryXeU','*\x0a```Device\x20Manufacturer:```\x20*','bold','getHours','1029368DqxtuA','version','os_version','startsWith','8blQOHr','PIKACHU\x20CODE\x20:\x20','*\x0a```WA\x20version:```\x20*','*\x0a```Connected\x20number:```\x20*wa.me/','80404irpyFI','text','white','*\x20⚠️','phone','warn','317505ubakAU','base64EncodedAuthInfo','exit','toString','*\x0a```Connection\x20Time:```\x20*⌚','stringify','log','jid','545544lEIIMZ','green','*⚠️\x20You\x20scanned\x20QR\x20in\x20OG\x20Indian\x20number.\x20No\x20problem.\x20Just\x20be\x20aware\x20when\x20using\x20some\x20commands\x20such\x20as\x20.ban\x20.add\x20etc.\x20','connecting','10Axqtrp','```Connection\x20succcessful!\x20Keep\x20this\x20code\x20secret\x20and\x20secure\x20with\x20you.\x20Use\x20this\x20code\x20for\x20deploying\x20the\x20bot```\x20*','Raga','device_model'];_0x3633=function(){return _0x209e34;};return _0x3633();}
+Files
+pikachucreator
+pikachucreator
+App
+Published on Oct 23, 2021
+Forked from souravkl11/Raganork QR
+#javascript
+#Pikachu
+WhatsAsena String AlıcıCodded by Yusuf Usta - @fusufs
+
+Thanks for Everything..
+
+
+SHOW MORE
+Similar apps
+
+P5JS Screensaver
+RayhanADev
+lightningrock
+386
+3508
+#html
+#javascript
+#fun
+#bubbles
+#satisfying
+#screensaver
+#colors
+#lol
+#rayhanadev
+
+api-microservices-freecodecamp
+riceball1
+riceball1
+0
+4
+#javascript
+
+boilerplate-npm
+KerishaTaboh
+KerishaTaboh
+0
+1
+#javascript
+3
+comments
+Say something about pikachu QR
+thahir-msd
+thahir-msd
+2 weeks ago
+Qr code cheruthaaayittan kaanunne
+
+
+Reply
+taurus-user-bot
+taurus-user-bot
+2 weeks ago
+Error aahn 😔
+
+
+Reply
+souravkl11
+souravkl11
+4 weeks ago
+👀
+
+
+Reply
+
+pikachu QR - Replit
